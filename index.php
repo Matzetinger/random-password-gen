a4a53e1d8e9133bd328fea767c2f727af0db1d9e
Matthias Mühlender <muehlenderm@googlemail.com>
2026-03-03 02:06:27 +0100

    Update

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php 
+
+if($_POST){
+  $chars = (int)$_POST["zeichenanzahl"];
+  $randomizer = new \Random\Randomizer();
+  $sourcestring = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  $randompassword = $randomizer->getBytesFromString($sourcestring, $chars);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +19,38 @@
   <title>Passwort Generator</title>
 </head>
 <body>
+
+<div class="title-container">
+  <h1>Passwort Generator</h1>
+  <h3>Dein Passwort</h3>
+</div>
+
+
+<div class="password-container">
+  
+  <h5><?php echo $randompassword ?? "Klick auf generieren"; ?></h5>
+</div>
+
+<form action="#" method="POST">
+  <div class="inputs-container">
+    <div class="input-container">
+      <label for="number">Wie viele Zeichen ?</label>
+      <input
+        class="number-input"
+        type="number"
+        name="zeichenanzahl"
+        value="8"
+        min="6"
+        max="15"
+        id="number"
+        require
+      >
+    </div>
+
+      <input class="submit-input" type="submit" value="Generieren">
+  </div>
+<a href="index.php">Zurücksetzen</a>
+</form>
   
 </body>
 </html>
